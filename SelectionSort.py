f63509ad67bec0df00ad8c2f847200864b320e89
Grey-Rabbit002 <arjun.saxena.as2002@gmail.com>
2022-04-27 09:56:16 +0530

    27-04-22 codes added to repository

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,7 +1,10 @@
 def selection(arr):
-    for i in range(arr):
+    for i in range(len(arr)):
         min=i
-        
-
+        for j in range(i+1,len(arr)):
+            if(arr[j]<arr[min]):
+                min=j
+        (arr[i],arr[min])=(arr[min],arr[i])
 arr=[2,3,8,5,9,3]
 selection(arr)
+print(arr)
